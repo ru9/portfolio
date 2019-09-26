598c7da8e44858cdb75ae96a2e0c91f4f72d60fd
ru9 <runisunde@gmail.com>
2019-09-26 21:40:11 +0200

    Scrolling project 1st itteration
    Scrolling project 1st itteration

---
+++ src/components/ContactForm.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="outer-container">
+    <form name="contact" method="POST" data-netlify="true">
+      <div>
+        <input type="text" name="name" placeholder="Fornavn" />
+      </div>
+      <div>
+        <input type="text" name="lastname" placeholder="Etternavn" />
+      </div>
+      <div>
+        <input type="email" name="email" placeholder="Email" />
+      </div>
+      <div>
+        <textarea name="message" placeholder="Melding"></textarea>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.outer-container {
+  margin: 4rem 0;
+}
+input,
+textarea {
+  padding: 1rem;
+  background-color: #ff6f002f;
+  font-size: 1rem;
+  color: black;
+  border: none;
+}
+</style>
