f433caf0a4812081bd85b66a1cca46017fe839a9
ru9 <runisunde@gmail.com>
2019-09-27 10:36:45 +0200

    tst form
    tst form

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -1,21 +1,25 @@
 <template>
-  <form name="contact" method="post" netlify>
-    <div>
-      <input type="text" name="firstname" placeholder="Fornavn" />
+  <div class="container">
+    <h1 class="title">Contact</h1>
+    <div class="content">
+      <form name="contact" action="/thank-you" netlify-honeypot="bot-field" method="post" netlify>
+        <input type="hidden" name="form-name" value="contact" />
+        <p class="hidden">
+          <label>
+            Don’t fill this out:
+            <input name="bot-field" />
+          </label>
+        </p>
+        <label class="form-label" for="name">Name:</label>
+        <input class="form-field" name="name" id="name" />
+        <label class="form-label" for="email">Email:</label>
+        <input class="form-field" name="email" id="email" />
+        <label class="form-label" for="message">Message:</label>
+        <textarea class="form-field" name="message" id="message"></textarea>
+        <input class="form-button" type="submit" value="Send message" />
+      </form>
     </div>
-    <div>
-      <input type="text" name="lastname" placeholder="Etternavn" />
-    </div>
-    <div>
-      <input type="email" name="email" placeholder="Email" />
-    </div>
-    <div>
-      <textarea name="message" placeholder="Melding"></textarea>
-    </div>
-    <div>
-      <button type="submit">Send</button>
-    </div>
-  </form>
+  </div>
 </template>
 
 <script>
