df321c685247a0b63870bda2edeb870ac4daf9f2
ru9 <runisunde@gmail.com>
2019-09-27 10:01:20 +0200

    ContactForm live tst
    ContactForm live tst

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -1,23 +1,21 @@
 <template>
-  <div class="outer-container">
-    <form name="contact" method="POST" data-netlify="true">
-      <div>
-        <input type="text" name="firstname" placeholder="Fornavn" />
-      </div>
-      <div>
-        <input type="text" name="lastname" placeholder="Etternavn" />
-      </div>
-      <div>
-        <input type="email" name="email" placeholder="Email" />
-      </div>
-      <div>
-        <textarea name="message" placeholder="Melding"></textarea>
-      </div>
-      <div>
-        <button type="submit">Send</button>
-      </div>
-    </form>
-  </div>
+  <form name="contact" method="POST" data-netlify="true">
+    <div>
+      <input type="text" name="firstname" placeholder="Fornavn" />
+    </div>
+    <div>
+      <input type="text" name="lastname" placeholder="Etternavn" />
+    </div>
+    <div>
+      <input type="email" name="email" placeholder="Email" />
+    </div>
+    <div>
+      <textarea name="message" placeholder="Melding"></textarea>
+    </div>
+    <div>
+      <button type="submit">Send</button>
+    </div>
+  </form>
 </template>
 
 <script>
