d02fe1e85644b05e8d8e6223f79c519848fb85ee
ru9 <runisunde@gmail.com>
2019-09-27 22:12:09 +0200

    Styling Contact form
    Styling Contact form

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -1,20 +1,24 @@
 <template>
-  <div>
+  <div class="outer-container">
+    <div>
+      <h1>Spørsmål?</h1>
+      <p>Spør oss gjerne om det du vil</p>
+    </div>
     <form name="contact" action="/thank-you" netlify-honeypot="bot-field" method="post" netlify>
       <input type="hidden" name="form-name" value="contact" />
       <input name="bot-field" hidden />
       <div>
-        <input type="text" name="firstname" placeholder="Fornavn" />
+        <input type="text" name="firstname" placeholder="Fornavn*" />
       </div>
       <div>
-        <input type="text" name="lastname" placeholder="Etternavn" />
+        <input type="text" name="lastname" placeholder="Etternavn*" />
       </div>
       <div>
-        <input type="email" name="email" placeholder="Email" />
+        <input type="email" name="email" placeholder="Email*" />
       </div>
       <div>
-        <select type="select" name="options" data-label="Options">
-          <option selected="Hvordan fant du oss?" value>Hvordan fant du oss?</option>
+        <select type="select" name="options">
+          <option value="Hvordan fant du oss?">Hvordan fant du oss?</option>
           <option value="Fra en venn">Fra en venn</option>
           <option value="Søk i google">Søk i google</option>
           <option value="Jeg er tidligere kunde">Jeg er tidligere kunde</option>
@@ -22,10 +26,10 @@
         </select>
       </div>
       <div>
-        <textarea name="message" placeholder="Melding"></textarea>
+        <textarea name="message" placeholder="Melding*"></textarea>
       </div>
       <div>
-        <input type="submit" value="Send message" />
+        <input class="not-italic send-button" type="submit" value="Send melding" />
       </div>
     </form>
   </div>
@@ -39,13 +43,56 @@ export default {};
 .outer-container {
   margin: 4rem 0;
 }
+form {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin: 0 auto;
+  width: 80vw;
+}
+select option {
+  color: rgb(255, 255, 255);
+}
+select option:first-child {
+  color: #ffffff;
+}
 input,
 textarea,
 select {
-  padding: 1rem;
-  background-color: #ff6f002f;
+  font-style: italic;
+  width: 100%;
+  padding: 0.9rem 1rem;
+  margin: 0.5rem 0;
+  background-color: #3a5d86;
   font-size: 1rem;
-  color: black;
+  font-family: "Quicksand", sans-serif;
   border: none;
+  color: white;
+}
+textarea {
+  height: 7rem;
+}
+::placeholder {
+  color: white;
+}
+.first-selected {
+  color: white;
+}
+form > div {
+  width: 80vw;
+  display: flex;
+  justify-content: center;
+}
+.not-italic {
+  font-style: normal;
+}
+.italic {
+  font-style: italic;
+}
+.send-button {
+  background-color: #ff6f00;
+  color: black;
+  font-size: 1.1rem;
+  font-weight: 900;
 }
 </style>
