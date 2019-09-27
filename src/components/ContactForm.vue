ff83c4cf730e2315e3cc8d07382c76c59800ec1c
ru9 <runisunde@gmail.com>
2019-09-27 11:30:32 +0200

    live tst form
    live tst form

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -1,24 +1,23 @@
 <template>
-  <div class="container">
-    <h1 class="title">Contact</h1>
-    <div class="content">
-      <form name="contact" action="/thank-you" netlify-honeypot="bot-field" method="post" netlify>
-        <input type="hidden" name="form-name" value="contact" />
-        <p class="hidden">
-          <label>
-            Don’t fill this out:
-            <input name="bot-field" />
-          </label>
-        </p>
-        <label class="form-label" for="name">Name:</label>
-        <input class="form-field" name="name" id="name" />
-        <label class="form-label" for="email">Email:</label>
-        <input class="form-field" name="email" id="email" />
-        <label class="form-label" for="message">Message:</label>
-        <textarea class="form-field" name="message" id="message"></textarea>
-        <input class="form-button" type="submit" value="Send message" />
-      </form>
-    </div>
+  <div>
+    <form name="contact" action="/thank-you" netlify-honeypot="bot-field" method="post" netlify>
+      <input name="bot-field" hidden />
+      <div>
+        <input type="text" name="firstname" placeholder="Fornavn" />
+      </div>
+      <div>
+        <input type="text" name="lastname" placeholder="Etternavn" />
+      </div>
+      <div>
+        <input type="email" name="email" placeholder="Email" />
+      </div>
+      <div>
+        <textarea name="message" placeholder="Melding"></textarea>
+      </div>
+      <div>
+        <input type="submit" value="Send message" />
+      </div>
+    </form>
   </div>
 </template>
 
